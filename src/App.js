1fca015c368b138e8b8005c26298411e0a1137d3
al-jabir <aljabir098@gmail.com>
2022-09-19 21:27:48 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 import Header from "./components/Header";
 
 const App = () => {
-  const contacts = [
-    {
-      id: 1,
-      name: "Knight",
-      email: "knight22@gmail.com",
-    },
-    {
-      id: 2,
-      name: "Sarah",
-      email: "sarah2@gmail.com",
-    },
-    {
-      id: 3,
-      name: "Jack",
-      email: "jackes@gmail.com",
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
+  const onContactHanlde = () => {
+    setContacts(contacts);
+  };
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact contactHandler={onContactHanlde} />
       <ContactList contacts={contacts} />
     </div>
   );
--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 
 class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+  };
+  add = (e) => {
+    e.preventDefault();
+    if (this.state.name === "" && this.state.email === "") {
+      alert("All the fields are madatory!");
+      return;
+    }
+
+    this.props.contactHandler(this.state);
+    console.log(this.state);
+  };
   render() {
     return (
       <div className="ui main">
         <h2>Add Contact</h2>
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.add}>
           <div className="field">
             <label>Name</label>
-            <input type="text" name="name" placeholder="Name" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
           </div>
           <div className="field">
             <label>Email</label>
-            <input type="text" name="email" placeholder="Email" />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
           </div>
           <button className="ui button green">Add</button>
         </form>
