2c0a0825a5d2af5c83a939d72c1f480259fcb86c
al-jabir <aljabir098@gmail.com>
2022-09-19 22:27:56 +0600

    I have done App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 import Header from "./components/Header";
 
 const App = () => {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
-  const onContactHanlde = () => {
-    setContacts(contacts);
+  const onContactHanlde = (contact) => {
+    setContacts([...contacts, contact]);
+    console.log(contact);
   };
+
+  const removeContactHandle = (id) => {
+    const newContactList = contacts.filter((li) => li.id !== id);
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
   return (
     <div className="ui container">
       <Header />
       <AddContact contactHandler={onContactHanlde} />
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} getContactId={removeContactHandle} />
     </div>
   );
 };
