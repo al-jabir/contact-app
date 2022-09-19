7eb6efe304bfad8a8b5e179cb21ea71ed8fc8cad
al-jabir <aljabir098@gmail.com>
2022-09-19 22:28:57 +0600

    I have done contactList

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ContactCard from "./ContactCard";
 
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, getContactId }) => {
+  const deleteItem = (id) => {
+    getContactId(id);
+  };
   return (
     <div className="ui celled list">
       {contacts.map((person, inx) => {
-        return <ContactCard person={person} key={inx} />;
+        return (
+          <ContactCard person={person} key={inx} clickRemove={deleteItem} />
+        );
       })}
     </div>
   );
