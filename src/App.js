795e2ca46ee25e6990ed9d0f440ac44433dd5cf8
al-jabir <aljabir098@gmail.com>
2022-09-19 21:05:25 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
 import "./App.css";
 import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
 import Header from "./components/Header";
 
 const App = () => {
+  const contacts = [
+    {
+      id: 1,
+      name: "Knight",
+      email: "knight22@gmail.com",
+    },
+    {
+      id: 2,
+      name: "Sarah",
+      email: "sarah2@gmail.com",
+    },
+    {
+      id: 3,
+      name: "Jack",
+      email: "jackes@gmail.com",
+    },
+  ];
   return (
     <div className="ui container">
       <Header />
       <AddContact />
+      <ContactList contacts={contacts} />
     </div>
   );
 };
