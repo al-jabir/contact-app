d0a6ff654c97bd6b529900bd9400218f45b69473
al-jabir <aljabir098@gmail.com>
2022-09-19 20:29:15 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Text........</h1>
+    <div>
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
